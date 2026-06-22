9c677e5774b3c1efe7eee68acc5ff367538d26b5
Thomas Rose <thomas.rose@ionos.com>
2026-06-22 08:47:15 +0200

    copy new files and workflows

--- cleanup.php
+++ cleanup.php
@@ -1,32 +1,47 @@
 <?php
-
 /**
- * Cleans up the environment after the test run.
+ * This script is responsible for cleaning up the test environment after a run of the WordPress PHPUnit Test Runner.
+ * It ensures that temporary directories and files created during the test process are properly deleted.
+ * 
+ * @link https://github.com/wordpress/phpunit-test-runner/ Original source repository
+ * @package WordPress
  */
-
 require __DIR__ . '/functions.php';
 
-// Check required environment variables.
+/**
+ * Check for the presence of required environment variables.
+ * This function should be defined in functions.php and should throw an
+ * exception or exit if any required variables are missing.
+ */
 check_required_env();
 
-// Bring some environment variables into scope.
-$WPT_PREPARE_DIR = getenv( 'WPT_PREPARE_DIR' );
-$WPT_SSH_CONNECT = getenv( 'WPT_SSH_CONNECT' );
-$WPT_SSH_OPTIONS = getenv( 'WPT_SSH_OPTIONS' ) ? : '-o StrictHostKeyChecking=no';
-$WPT_TEST_DIR = getenv( 'WPT_TEST_DIR' );
-$WPT_RM_TEST_DIR_CMD = getenv( 'WPT_RM_TEST_DIR_CMD' ) ? : 'rm -r ' . $WPT_TEST_DIR;
+/**
+ * Ensure that all environment variables are present with default values.
+ */
+$runner_vars = setup_runner_env_vars();
 
-// Clean up the preparation directory.
-// Only forcefully delete the .git directory, to prevent disasters otherwise.
+/**
+ * The directory path of the test preparation directory is assumed to be previously defined.
+ * For example: $runner_vars['WPT_PREPARE_DIR'] = '/path/to/your/preparation/dir';
+ * Clean up the preparation directory.
+ * Forcefully deletes only the .git directory and the node_modules cache.
+ * Afterward, the entire preparation directory is removed to ensure a clean state for the next test run.
+ */
 perform_operations( array(
-	'rm -rf ' . escapeshellarg( $WPT_PREPARE_DIR . '/.git' ),
-	'rm -rf ' . escapeshellarg( $WPT_PREPARE_DIR . '/node_modules/.cache' ),
-	'rm -r ' . escapeshellarg( $WPT_PREPARE_DIR ),
+	'rm -rf ' . escapeshellarg( $runner_vars['WPT_PREPARE_DIR'] . '/.git' ),
+	'rm -rf ' . escapeshellarg( $runner_vars['WPT_PREPARE_DIR'] . '/node_modules/.cache' ),
+	'rm -r ' . escapeshellarg( $runner_vars['WPT_PREPARE_DIR'] ),
 ) );
 
-// Clean up the test directory in remote environments.
-if ( ! empty( $WPT_SSH_CONNECT ) ) {
+/**
+ * Cleans up the test directory on a remote server.
+ * This conditional block checks if an SSH connection string is provided and is not empty.
+ * If a connection string is present, it triggers a cleanup operation on the remote environment.
+ * The cleanup operation is executed by the `perform_operations` function which takes an array
+ * of shell commands as its input.
+ */
+if ( ! empty( $runner_vars['WPT_SSH_CONNECT'] ) ) {
 	perform_operations( array(
-		'ssh ' . $WPT_SSH_OPTIONS . ' ' . escapeshellarg( $WPT_SSH_CONNECT ) . ' ' . escapeshellarg( $WPT_RM_TEST_DIR_CMD ),
+		'ssh ' . $runner_vars['WPT_SSH_OPTIONS'] . ' ' . escapeshellarg( $runner_vars['WPT_SSH_CONNECT'] ) . ' ' . escapeshellarg( $runner_vars['WPT_RM_TEST_DIR_CMD'] ),
 	) );
 }
